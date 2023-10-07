115a85f5f725d762df15a38a4c5c80207923b9c7
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-10-08 00:38:13 +0530

    Update Is A Number Prime.cpp

--- Is A Number Prime.cpp
+++ Is A Number Prime.cpp
@@ -10,9 +10,9 @@ int main() {
     res = 0;
   }
 
-	for (int i = 2; i <= n / 2; i++) {
-		if (n % i == 0) {
-			res = 0;
+  for (int i = 2; i <= n / 2; i++) {
+    if (n % i == 0) {
+      res = 0;
       break;
     }
   }
