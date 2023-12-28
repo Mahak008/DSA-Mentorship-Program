a608ca6cfc9878897e959f37a6b0da517e1777f4
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-12-28 20:17:02 +0530

    Update Inverse a Number.cpp

--- Assignment1/Inverse a Number.cpp
+++ Assignment1/Inverse a Number.cpp
@@ -6,7 +6,8 @@ int main() {
 
   while(num) {
     int rem = num % 10; 
-    sum = sum +  place*(pow(10, rem-1)); // to put 1 at 5th place, we need to concat 4 0s after 1. Similarly to put 2 at 3rd place, we need to concat 2 0s after 2 and so on.
+    sum = sum +  place*(pow(10, rem-1)); 
+    // to put 1 at 5th place, we need to concat 4 0s after 1. Similarly to put 2 at 3rd place, we need to concat 2 0s after 2 and so on.
     num /= 10;
     place++;
   }
