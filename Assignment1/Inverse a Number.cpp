7aca7cfb077043617d089e9e7a61843f67c496c5
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-12-28 21:13:07 +0530

    Update Inverse a Number.cpp

--- Assignment1/Inverse a Number.cpp
+++ Assignment1/Inverse a Number.cpp
@@ -1,3 +1,8 @@
+// The inverse of a number is defined as the number created by interchanging the face value and index of digits of number.
+// e.g. for 426135 (reading from right to left, 5 is in place 1, 3 is in place 2, 1 is in place 3, 6 is in place 4, 2 is in place 5 
+// and 4 is in place 6), the inverse will be 416253 (reading from right to left, 3 is in place 1, 5 is in place 2,2 is in place 3, 
+// 6 is in place 4, 1 is in place 5 and 4 is in place 6) More examples - inverse of 2134 is 1243 and inverse of 24153 is 24153.    
+
 int main() {
   int num;
   cin >> num;
