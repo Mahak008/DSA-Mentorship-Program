3c100e70d150826f18b23f1305644bc3060e99a6
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2023-12-30 14:12:18 +0530

    Update Pattern 5.cpp

--- Assignment2/Pattern 5.cpp
+++ Assignment2/Pattern 5.cpp
@@ -1,15 +1,20 @@
-int n = 5;
-for(int i = 0; i < n; i++) {
-  for(int j = 0; j < n; j++) {
-    if(i == n/2 || j == n/2) {
-      cout << "* ";
-    }
-    else if ((i != 0 && i != n-1) && (i == j || j == n-i-1)) {
-      cout << "* ";
-    }
-    else {
-      cout << "  ";
-    }
+int n = 3, i, j, k;
+for(i = 0; i < n; i++) {
+  for(j = 0; j < (n-i); j++) {
+    cout << " ";
+  }
+  for(k = 0; k < i*2+1; k++) {
+    cout << "*";
+  }
+  cout << endl;
+}
+    
+for(i = n-1; i > 0; i--) {
+  for(j = 0; j <= (n-i); j++) {
+    cout << " ";
+  }
+  for(k = i*2-1; k > 0; k--) {
+    cout << "*";
   }
   cout << endl;
 }
