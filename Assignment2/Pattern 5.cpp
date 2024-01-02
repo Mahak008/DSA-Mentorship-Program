53bea387f398dc86d22d9efc2b43b03140ec6d42
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-02 13:46:10 +0530

    Update Pattern 5.cpp

--- Assignment2/Pattern 5.cpp
+++ Assignment2/Pattern 5.cpp
@@ -1,3 +1,33 @@
+int n = 3;
+int spaces = n;
+int stars = 1;
+
+for(int i = 1; i <= 2*n-1; i++) {
+
+    // spaces
+    for(int j = 1; j <= spaces; j++) {
+        cout << " ";
+    }
+    
+    //spaces
+    for(int k = 0; k < stars; k++) {
+        cout << "*";
+    }
+
+    if(i < n) {
+        spaces--;
+        stars+=2;
+    }
+    else {
+        spaces++;
+        stars -= 2;
+    }
+
+    cout <<endl;
+}
+
+// Another Method
+
 int n = 3, i, j, k;
 for(i = 0; i < n; i++) {
   for(j = 0; j < (n-i); j++) {
