f7385cac8d5e2919a4c85523583d3855e8279f8a
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-02 13:51:18 +0530

    Update Pattern 6.cpp

--- Assignment2/Pattern 6.cpp
+++ Assignment2/Pattern 6.cpp
@@ -1,24 +1,19 @@
 int n = 3;
 int spaces = n;
 int stars = 1;
-
 for(int i = 1; i <= 2*n-1; i++) {
-
     // stars
     for(int j = 1; j <= spaces; j++) {
         cout << "*";
     }
-    
     //spaces
     for(int k = 0; k < stars; k++) {
         cout << " ";
     }
-
     // stars
     for(int j = 1; j <= spaces; j++) {
         cout << "*";
     }
-
     if(i < n) {
         spaces--;
         stars+=2;
@@ -27,12 +22,12 @@ for(int i = 1; i <= 2*n-1; i++) {
         spaces++;
         stars -= 2;
     }
-
     cout <<endl;
 }
 
 // Another Method
 
+// Upper Symmetry
 int n = 3, i, j, spaces = 1;
 for(i = 0; i < n; i++) {
 
@@ -54,6 +49,7 @@ for(i = 0; i < n; i++) {
   cout << endl;
 }
 
+// Lower Symmetry
 spaces = 2*n-2;
 for(i = 1; i < n; i++) {
 
