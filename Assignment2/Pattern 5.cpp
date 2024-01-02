67d11e4e34f4361f13c2463395bbd3b2921701d1
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-02 13:50:32 +0530

    Update Pattern 5.cpp

--- Assignment2/Pattern 5.cpp
+++ Assignment2/Pattern 5.cpp
@@ -1,19 +1,15 @@
 int n = 3;
 int spaces = n;
 int stars = 1;
-
 for(int i = 1; i <= 2*n-1; i++) {
-
     // spaces
     for(int j = 1; j <= spaces; j++) {
         cout << " ";
     }
-    
     //stars
     for(int k = 0; k < stars; k++) {
         cout << "*";
     }
-
     if(i < n) {
         spaces--;
         stars+=2;
@@ -22,12 +18,12 @@ for(int i = 1; i <= 2*n-1; i++) {
         spaces++;
         stars -= 2;
     }
-
     cout <<endl;
 }
 
 // Another Method
 
+// Upper Triangle
 int n = 3, i, j, k;
 for(i = 0; i < n; i++) {
   for(j = 0; j < (n-i); j++) {
@@ -38,7 +34,7 @@ for(i = 0; i < n; i++) {
   }
   cout << endl;
 }
-    
+// Lower Triangle    
 for(i = n-1; i > 0; i--) {
   for(j = 0; j <= (n-i); j++) {
     cout << " ";
