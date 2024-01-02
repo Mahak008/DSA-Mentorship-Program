69456826816a3572dba69938c06001e18b5a782e
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-01-02 14:18:40 +0530

    Update Pattern 12.cpp

--- Assignment2/Pattern 12.cpp
+++ Assignment2/Pattern 12.cpp
@@ -1,13 +1,17 @@
-int n = 4, i, j, num = 1;
-for(i = 0; i < n; i++) {
-  for(j = 0; j <= i; j++) {
-    cout << num++ << " ";
-  }
-  cout << endl;
+int n = 5, a = 0, b = 1, c;
+
+for(int i = 0; i < n; i++) {
+    for(int j = 0; j < i; j++) {
+        c = a;
+        cout << c << " ";
+        a += b;
+        b = c;
+    }
+    cout <<endl;
 }
 
 // Output -
-// 1 
-// 2 3 
-// 4 5 6 
-// 7 8 9 10 
+// 0 
+// 1 1 
+// 2 3 5 
+// 8 13 21 34 
