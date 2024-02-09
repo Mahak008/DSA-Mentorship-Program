e2a58dadbdce4d3ab95a36b76f314c2f1767f997
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-02-10 03:50:31 +0530

    Create Linear Search.cpp

---
+++ Assignment 3/Linear Search.cpp
@@ -0,0 +1,10 @@
+int linearSearch(int n, int num, vector<int> &arr) {
+    int ans = -1;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == num) {
+            ans = i;
+            break;
+        }
+    }
+    return ans;
+}
