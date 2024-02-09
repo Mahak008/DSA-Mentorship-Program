11078df2199cb04fe3d8f9071ac44aae1113477f
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-02-10 03:53:02 +0530

    Create Missing Number.cpp

---
+++ Assignment 3/Missing Number.cpp
@@ -0,0 +1,10 @@
+int missingNumber(vector<int>& nums) {
+  int xor1 = 0, xor2 = 0;
+  int n = nums.size()-1;
+  for(int i = 0; i<n; i++) {
+    xor2 = xor2 ^ nums[i];
+    xor1 = xor1 ^ (i+1);
+  }
+  xor1 = xor1 ^ nums.size();
+  return xor1 ^ xor2;
+}
