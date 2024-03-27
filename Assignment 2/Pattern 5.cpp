9c64bbf6b86793393ce3aff10e34e2901603119f
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-03-27 13:48:03 +0530

    Create Pattern 5.cpp

---
+++ Assignment 2/Pattern 5.cpp
@@ -0,0 +1,53 @@
+int n = 3;
+int spaces = n;
+int stars = 1;
+for(int i = 1; i <= 2*n-1; i++) {
+    // spaces
+    for(int j = 1; j <= spaces; j++) {
+        cout << " ";
+    }
+    //stars
+    for(int k = 0; k < stars; k++) {
+        cout << "*";
+    }
+    if(i < n) {
+        spaces--;
+        stars+=2;
+    }
+    else {
+        spaces++;
+        stars -= 2;
+    }
+    cout <<endl;
+}
+
+// Another Method
+
+// Upper Triangle
+int n = 3, i, j, k;
+for(i = 0; i < n; i++) {
+  for(j = 0; j < (n-i); j++) {
+    cout << " ";
+  }
+  for(k = 0; k < i*2+1; k++) {
+    cout << "*";
+  }
+  cout << endl;
+}
+// Lower Triangle    
+for(i = n-1; i > 0; i--) {
+  for(j = 0; j <= (n-i); j++) {
+    cout << " ";
+  }
+  for(k = i*2-1; k > 0; k--) {
+    cout << "*";
+  }
+  cout << endl;
+}
+
+// Output -
+//     *     
+//   * * *   
+// * * * * * 
+//   * * *   
+//     *     
