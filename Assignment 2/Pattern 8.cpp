7232317821997926b724aefb9b0800414957f257
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-03-27 13:49:21 +0530

    Create Pattern 8.cpp

---
+++ Assignment 2/Pattern 8.cpp
@@ -0,0 +1,18 @@
+for(i = 0; i < 5; i++) {
+  for(j = 0; j < 5; j++) {
+    if(i == j) {
+      cout << "* ";
+    }
+    else {
+      cout <<"  ";
+    }
+  }
+  cout << endl;
+}
+
+// Output -
+// *         
+//   *       
+//     *     
+//       *   
+//         * 
