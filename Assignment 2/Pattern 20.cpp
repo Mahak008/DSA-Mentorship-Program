231f412c6695a244861bcc5f03a5ebe4d27717fd
Mahak Garg <88129955+Mahak008@users.noreply.github.com>
2024-03-27 13:46:48 +0530

    Create Pattern 20.cpp

---
+++ Assignment 2/Pattern 20.cpp
@@ -0,0 +1,19 @@
+int n = 5;
+for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+        if((j == 0 || j == n-1) || ( i >= n /2 && (i == j || i+j == n-1))) {
+            cout << "* ";
+        }
+        else {
+            cout << "  ";
+        }
+    }
+    cout << endl;
+}
+
+// Output - 
+// *       * 
+// *       * 
+// *   *   * 
+// * *   * * 
+// *       * 
